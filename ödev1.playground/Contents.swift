fe659e4d1b3eba8253980ab1256e7e4bcc7eb6b8
cananyavuz <yavuzcanan15@gmail.com>
2024-10-15 23:43:45 +0300

    odev1 done

---
+++ ödev1.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+//1. gün ödev - Değişken Oluşturma
+
+var ilce : String = "Merkez"
+var kita : String = "Asya"
+var faks : String = "+90-258-9998888"
+var postaKodu : String = "53100"
+var instaAdresi : String = "username01"
+var calistiginBolum : String = "Software Development"
+var urunMiktari : Int = 437535
+var musteriSoyadı : String = "Yavuz"
+var odemeMiktari : Double = 23.55
+var dogumTarihi : String = "01.01.01"
+var borc : Double = 2056.50
+var medeniHal : String = "bekar"
+var videoYorumu : String = "Çok eğlenceli bir video."
+
+var odemeSaati : String = "12:00"
+var eftMiktari : Double = 56.75
+var satilanMiktar : Int = 54
+var telefonModeli : String = "iPhone 11"
+var dergiAdi : String = "Kafa"
+var basimTarihi : String = "2019"
+var zamMiktari : Int = 4000
+var daireSayisi : Int = 54
+var enlem : Double = 42.00
+var boylam : Double = 45.00
+var yemekAdi : String = "Kebap"
+var urunFiyati : Double = 12.50
+
+var sirket : String = "Apple"
+var videoAdi : String = "IOS PROGRAMLAMAYA GİRİŞ"
+var müzikSuresi : String = "03:45"
+var mekanPuani : Double = 4/5
+var dosyaAdi : String = "ödev.jpg"
+var resimFormati : String = "jpg"
+var renk : String = "red"
+var renkKodu : String = "#ff0000"
+var bilgisayarModeli : String = "MacBook Air"
+var ekranBoyutu : String = "13.6 inç"
+var kullanımSuresi : String = "26:45:34:12"
+
+var basinc : String = "760 mmHg"
+var etkinlikGünü : String = "2 Ekim 2024 Çarşamba"
+var odemeGünü : String = "03/03/16"
+var yolculukCikisTarihi : String = "13/05/17"
+var mahalleAdi : String = "Sultan Selim"
+var otobusHatti : String = "27E"
+var kullanilanDakika : String = "12:00:00"
+var kargoTakipNo : String = "123456789"
+var kuponSuresi : String = "12:00:00"
+var kuponKodu : String = "123456789"
+var faturaTarihi : String = "12/01/16"
+
